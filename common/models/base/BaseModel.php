add2927e36ad9070d7db003d766a936c24239482
wfnking <wfnking@sina.com>
2017-02-23 13:47:56 +0800

    3.4 add PostModel and PostForm

---
+++ common/models/base/BaseModel.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace common\models\base;
+
+use yii\db\ActiveRecord;
+
+/**
+*基础模型
+*/
+class BaseModel extends ActiveRecord
+{
+
+}
--- frontend/controllers/base/BaseController.php
+++ frontend/controllers/base/BaseController.php
@@ -4,7 +4,7 @@ namespace frontend\controllers\base;
 use yii\web\Controller;
 
 /**
- * 基础控制器
+ *基础控制器
  */
 class BaseController extends Controller
 {
